f8f6e9d55d75a063dc14a0310dfbcbe8009dd237
Harsh Gupta <harshsgupta1999@gmail.com>
2023-06-09 21:21:00 +0530

    I/O file commit

---
+++ in_out_syntax.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    '''x = open('C:\\Users\\harsh_ph1dmia\\Desktop\\file.txt')
+    print(x.read())
+    x.seek(0)  # when we read the file curser goes to the end of the file so we need to reset the cursor
+    print(x.readline())  # read a single line
+    x.seek(0)
+    print(x.readlines())  # read all the line in line format
+    x.close()
+    with open('C:\\Users\\harsh_ph1dmia\\Desktop\\file.txt')as f:
+        contents = f.read()
+    print(contents)
+    with open('C:\\Users\\harsh_ph1dmia\\Desktop\\file.txt', mode='r')as f:
+        contents = f.read()
+    print(contents)'''
+    with open('C:\\Users\\harsh_ph1dmia\\Desktop\\file.txt', mode='a')as f:
+        contents = f.write('/nThis is append')
